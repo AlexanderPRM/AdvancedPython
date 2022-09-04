b25efddeba4f403f6f31069042578b6995987268
Александр Пермяков <nathan920@yandex.ru>
2022-09-04 13:38:17 +0300

    Small Update

--- Web-Scraping HW3/config.py
+++ Web-Scraping HW3/config.py
@@ -1,7 +1,7 @@
 import requests as req
 from bs4 import BeautifulSoup
 from fake_headers import Headers
-
+from pprint import pprint
 
 def habr_parser(KEYWORDS):
     url = 'https://habr.com/ru/all/'
@@ -16,21 +16,22 @@ def habr_parser(KEYWORDS):
 
     for article in list_article:
         hubs = article.find_all(class_= 'tm-article-snippet__hubs-item')
-        hubs = [hub.text.strip() for hub in hubs]
-        title = article.find_all(class_= 'tm-article-snippet__title tm-article-snippet__title_h2')
-        title = [title.text.strip() for title in title]
+        hubs = [hub.text.lower().strip() for hub in hubs]
+        titles = article.find_all(class_= 'tm-article-snippet__title tm-article-snippet__title_h2')
+        title = [title.text.lower().strip() for title in titles]
+        title_output = [title.text.strip() for title in titles] # Данная переменная для вывода заголовка в исходном виде
+
         # На разных статьях иногда превью текста находится в разных тегах, из-за этого чтобы избавиться от потери данных
         # Создал две переменные и их будем проверять.
         descriptions = article.find_all(class_= 'article-formatted-body article-formatted-body article-formatted-body_version-2')
         descriptions_2 = article.find_all(class_= 'article-formatted-body article-formatted-body article-formatted-body_version-1')
-        descriptions = [description.text.strip() for description in descriptions]
-        descriptions_2 = [description.text.strip() for description in descriptions_2]
+        descriptions = [description.text.lower().strip() for description in descriptions]
+        descriptions_2 = [description.text.lower().strip() for description in descriptions_2]
         all_info = hubs + title + descriptions + descriptions_2
-
         for word in KEYWORDS:
             if word in all_info or word + ' *' in all_info:
                 href = 'https://habr.com' + article.find(class_= 'tm-article-snippet__title-link').attrs['href']
                 published_time = article.find(class_= 'tm-article-snippet__datetime-published').text.strip()
-                print(f'Статья найдена по ключевому слову - {word}\nЗаголовок: {title[0]}\n\n{published_time}\nСсылка на статью - {href}')
+                print(f'Статья найдена по ключевому слову - {word}\nЗаголовок: {title_output[0]}\n\n{published_time}\nСсылка на статью - {href}')
                 print("-" * 60)
 
